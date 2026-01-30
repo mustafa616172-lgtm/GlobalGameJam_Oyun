4cb59b63e854a2a5b884c70ac3075b05c6f882e8
bedirksknn <63459698+bedirksknn@users.noreply.github.com>
2026-01-30 23:27:02 +0300

    ana menu

--- Assets/AnaMenu/Menu.cs
+++ Assets/AnaMenu/Menu.cs
@@ -1,16 +1,65 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class Menu : MonoBehaviour
+public class MainMenuManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [Header("Sahne Ayarları")]
+    public string gameSceneName = "SampleScene"; // Oyunun ana sahnesinin adı
+    public GameObject optionsPanel; // Ayarlar penceresi (Panel)
+
+    // Kayıt anahtarı (Veritabanındaki değişken ismi gibi düşün)
+    private const string SAVE_KEY = "HasSavedGame"; 
+
+    // 1. YENİ OYUN BUTONU
+    public void NewGame()
     {
+        // 1 = Hikayeyi Göster demektir
+        PlayerPrefs.SetInt("ShowIntroStory", 1); 
         
+        // Yeni oyun olduğu için kayıt var mı bilgisini oluştur
+        PlayerPrefs.SetInt("HasSavedGame", 1);
+        PlayerPrefs.Save();
+
+        SceneManager.LoadScene(gameSceneName);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void ContinueGame()
     {
-        
+        if (PlayerPrefs.HasKey("HasSavedGame"))
+        {
+            // Devam ettiği için hikayeyi GÖSTERME (0)
+            PlayerPrefs.SetInt("ShowIntroStory", 0); 
+            
+            SceneManager.LoadScene(gameSceneName);
+        }
+        else
+        {
+            // Kayıt yoksa yeni oyun gibi davran
+            NewGame();
+        }
+    }
+
+    // 3. AYARLAR BUTONU
+    public void OpenOptions()
+    {
+        if (optionsPanel != null)
+        {
+            optionsPanel.SetActive(true); // Paneli aç
+        }
+    }
+
+    public void CloseOptions() // Ayarlar panelindeki 'Geri' butonu için
+    {
+        if (optionsPanel != null)
+        {
+            optionsPanel.SetActive(false); // Paneli kapat
+        }
+    }
+
+    // 4. ÇIKIŞ BUTONU
+    public void QuitGame()
+    {
+        Debug.Log("Oyundan Çıkıldı!");
+        Application.Quit();
     }
 }
